5599d976ef84c2524df40e43d684cb2ddf3feacc
example00example <example00example@gmail.com>
2015-09-09 09:40:56 +0600

    Revert "Revert "New unit""
    This reverts commit 530aa69f557bc5e06423e18fc647fef042755dfb.

--- Untitled2.cpp
+++ Untitled2.cpp
@@ -5,8 +5,6 @@ using namespace std;
 int main (int argc, char *argv[])
 {
   cout << "Hello World!" << endl;
-  cout << "My name DK!" << endl;
-  cout << "WWWWWW DK!" << endl;
   cout << "Press ENTER to continue..." << endl; 
   cin.get();
   return 0;
