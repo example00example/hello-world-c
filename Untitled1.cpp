aaf22cc7869995732fb4a190c5bfa7ec34de1068
example00example <example00example@gmail.com>
2015-09-09 09:11:42 +0600

    My name
    Added my name

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main (int argc, char *argv[])
 {
   cout << "Hello World!" << endl;
+  cout << "My name DK!" << endl;
   cout << "Press ENTER to continue..." << endl; 
   cin.get();
   return 0;
